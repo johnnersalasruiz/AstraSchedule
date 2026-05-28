9fda19599c14e31d85c2839514211256f538e08d
Estebansala <juanestebansalazarquijano@gmail.com>
2026-05-28 00:16:06 -0500

    Fix tools/index.js, add nodemailer, configuracion inicial

--- tools/index.js
+++ tools/index.js
@@ -9,7 +9,7 @@ export { evaluar_fusion_grupos } from './generacion/evaluar_fusion_grupos.js';
 export { proponer_horario } from './generacion/proponer_horario.js';
 export { asignar_clase } from './generacion/asignar_clase.js';
 export { generar_horarios_pendientes } from './generacion/generar_horarios_pendientes.js';
-export { detectar_conflictos_bloque } from './generacion/detectar_conflictos_bloque.js';
+// export { detectar_conflictos_bloque } from './generacion/detectar_conflictos_bloque.js';
 
 // Notificaciones
 export { notificar_docente } from './notificaciones/notificar_docente.js';
